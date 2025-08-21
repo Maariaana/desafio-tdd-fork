d7f666aeca482dd291a30c6c91d7854999c9641f
Unipar <marianadossantosrubim@gmail.com>
2025-08-20 21:10:28 -0300

    Eu passei até o 6

--- venda.js
+++ venda.js
@@ -1,6 +1,36 @@
-
-
 function calcularVenda(venda) {
-  // Implementação da função
+  let subtotal = 0;
+
+  venda.itens.forEach(item => {
+    const valor = item.valorItem || 0;
+
+    const descontoPercentual = item.descontoPercentual
+      ? (valor * item.descontoPercentual) / 100
+      : 0;
+
+    const descontoReal = item.descontoReal || 0;
+
+    const descontoAplicado = Math.max(descontoPercentual, descontoReal);
+
+    subtotal += valor - descontoAplicado;
+  });
+
+  const descontoPorPercentual = venda.descontoTotalPercentual
+    ? (subtotal * venda.descontoTotalPercentual) / 100
+    : 0;
+
+  const descontoPorReal = venda.descontoTotalReal || 0;
+
+  const descontoFinal = Math.max(descontoPorPercentual, descontoPorReal);
+
+  const totalFinal = subtotal - descontoFinal;
+
+  return {
+    subtotal: Number(subtotal.toFixed(2)),
+    descontoTotalPercentual: venda.descontoTotalPercentual,
+    descontoTotalReal: venda.descontoTotalReal,
+    totalFinal: Number(totalFinal.toFixed(2)),
+  };
 }
+
 module.exports = calcularVenda;
